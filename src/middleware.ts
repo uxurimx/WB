ac93d923de0f55d6fee28855069f0878b2b2809a
tekton_dev <tekton.dev@outlook.com>
2026-05-21 19:05:11 -0600

    analitics v2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/', '/login', '/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)', '/api/uploadthing(.*)', '/offline']);
+const isPublicRoute = createRouteMatcher(['/', '/login', '/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)', '/api/uploadthing(.*)', '/offline', '/api/analytics/session', '/api/analytics/event']);
 
 export default clerkMiddleware(async (auth, request) => {
     if (!isPublicRoute(request)) {
